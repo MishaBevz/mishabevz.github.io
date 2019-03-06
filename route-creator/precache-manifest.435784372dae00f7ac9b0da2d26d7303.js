54e981a53a67bc0511be795658d8805a5c53ea9d
MishaBevz <misha.bevzenko@yandex.ru>
2019-03-06 18:24:31 +0300

    Add files via upload

---
+++ route-creator/precache-manifest.435784372dae00f7ac9b0da2d26d7303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9217e21b654dc0a268d69054934240f",
+    "url": "./static/media/marker.f9217e21.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c05f145fee58720b12ae",
+    "url": "./static/js/main.c05f145f.chunk.js"
+  },
+  {
+    "revision": "3fcc0a2f49afa87ab137",
+    "url": "./static/js/1.3fcc0a2f.chunk.js"
+  },
+  {
+    "revision": "c05f145fee58720b12ae",
+    "url": "./static/css/main.26ecfc93.chunk.css"
+  },
+  {
+    "revision": "34f324668fd40d87c7233ca120f89ab8",
+    "url": "./index.html"
+  }
+];
